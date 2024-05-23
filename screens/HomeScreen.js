a7969c3600ddf79e196a35dd66af81f6fe46aad6
Anusha Kannapiran <anushakannapiran@Anushas-MacBook-Pro.local>
2024-05-23 22:55:45 +0800

    HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,38 @@
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import QRCode from 'react-native-qrcode-svg';
 
-function HomeScreen() {
+const HomeScreen = () => {
+  const [currentNumber, setCurrentNumber] = useState(1);
+
+  const generateNextNumber = () => {
+    if (currentNumber < 100) {
+      setCurrentNumber(currentNumber + 1);
+    }
+  }
+  const generatePreviousNumber = () => {
+    if (currentNumber > 1 ) {
+      setCurrentNumber(currentNumber - 1);
+    }
+  };
+
   return (
-    <View>
-      <Text>This is Home Screen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ marginBottom: 20 }}>Book Id: {currentNumber}</Text>
+      <QRCode
+        value={currentNumber.toString()}
+        size={200}
+        color="black"
+        backgroundColor="white"
+      />
+      <View style={{ flexDirection:"row", padding:10}}> 
+      <Button title="<" onPress={generatePreviousNumber} disabled={currentNumber === 0} />
+      <Button title=">" onPress={generateNextNumber} disabled={currentNumber === 100} />
+      </View>
     </View>
-  );
-}
+  )
+};
+
 
 export default HomeScreen;
+
